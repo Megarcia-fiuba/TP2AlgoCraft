73e14cc2f317388ece4fe62768d67fece156656e
Garcia <matias-ezequiel.garcia@capgemini.com>
2019-06-02 14:05:56 -0300

    probando travis, pruebas fallan

---
+++ test/temporal/TemporalTest.java
@@ -0,0 +1,12 @@
+package temporal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TemporalTest {
+
+    @Test
+    public void TestFallantdo(){
+        Assert.assertTrue(false);  
+    }
+}
